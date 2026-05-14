cedc566d0a19bba86df5c01d4c6b4c287c4a4017
preventiugeneralitat-creator <preventiugeneralitat@gmail.com>
2026-05-14 18:08:47 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,20 @@
-const CACHE_NAME = 'precursor-app-v1';
-
+const CACHE_NAME = 'precursorado-v1';
 const urlsToCache = [
   './',
   './index.html',
-  './s-205b_s.pdf',
+  './planes.png',
   './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  './s-205b_s.pdf'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
